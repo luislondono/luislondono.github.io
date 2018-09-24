d475541c49bece7f6cf8a485418f669d276c48be
luislondono <luislondono.co@gmail.com>
2018-09-24 16:05:54 -0400

    testing hover javascript

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function hover(description, description_id) {
         document.getElementById(description_id).innerHTML = description;
         console.log("Hover is being executed")
 }
+
+function passwordProtect(){
+  console.log("Password form is being executed")
+  var password;
+  var correctpassword = "pleaseandthankyou";
+  password= prompt('Please enter in the password to view this page', '');
+  if (password == correctpassword)
+  alert('Password is correct. Click ok to enter...');
+  else {
+    window.location="resume"
+  }
+}
+
+function foo(){
+  alert('foo is being executed');
+  console.log('foo is being executed');
+}
