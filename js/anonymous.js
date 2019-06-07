ec79d09526ca552793b15dbf9a7323a64fe2202a
Luis Londono <luis.londono@uber.com>
2019-06-06 17:03:56 -0700

    some pushes

--- js/anonymous.js
+++ js/anonymous.js
@@ -1,53 +1,111 @@
 window.onload = updateMessageBoard
 let messageBoard;
+let ipHistory;
 let messageBoardPosts = []
 let clientInfo;
 let numQuestionsToday = 0;
+upvoteColor = [255, 174, 26]
+downvoteColor = [144, 215, 243]
 const emptyMessageBoardElement = '<div class = "message-board-post"><h3> No posts have been made :( </h3></div>'
 
 
 
 async function updateMessageBoard() {
-  // getClientInfo()
+
+
   messageBoard = firebase.firestore().collection("anonymousMessageBoard")
+  ipHistory = firebase.firestore().collection("ipVotingHistory")
 
-  await messageBoard.orderBy("date").get()
-    .then(querySnapshot => {
-      querySnapshot.docs.forEach(doc => {
-        messageBoardPosts.push(doc.data());
-      });
-    }).then(getClientInfo()).then(function () {
-      console.log("Line 20:")
-      console.log(clientInfo)
-
-      if (messageBoardPosts.length == 0) {
-        document.getElementById("message-board-posts").innerHTML = emptyMessageBoardElement
-      }
-      else {
-        messageBoardPosts.forEach(post => {
-          addPostToMessageBoard(post)
-          if(post["ip"] == clientInfo["ip"] && sameDay(Date.parse(post["date"]).toGMTString(), Date()) ) {
-            numQuestionsToday += 1;
-          }
+  await messageBoard.orderBy("postScore", "desc").orderBy("dateValue").get()
+    .then(
+      querySnapshot => {
+        querySnapshot.docs.forEach(doc => {
+          messageBoardPosts.push(doc.data());
         });
+        getClientInfo()
       }
+    ).then(
+      function () {
+        if (messageBoardPosts.length == 0) {
+          document.getElementById("message-board-posts").innerHTML = emptyMessageBoardElement
+          // getClientInfo()
+        }
+        else {
+          messageBoardPosts.forEach(post => {
+            addPostToMessageBoard(post, false)
+          })
+          console.log("Done adding all posts")
+          // getClientInfo()
+          // console.log(clientInfo)
+
+          messageBoardPosts.forEach(post => {
+            IPsMatch = post["ip"] == clientInfo["ip"]
+            daysMatch = sameDay(new Date(Date.parse(post["date"])), new Date())
+            if (IPsMatch && daysMatch) {
+              numQuestionsToday += 1;
+            }
+
+            renderActiveVoteButtons(post)
 
 
-    })
+          })
+        }
+
+      }
+    )
+
+
+}
+
+function renderActiveVoteButtons(postObject) {
+  // try {
+  //   postWasUpvoted = postObject["upvoters"].indexOf(clientInfo["ip"]) > -1
+  // }
+  // finally {
+  //   postWasUpvoted = false
+  // }
+  // try {
+  //   postWasDownvoted = postObject["downvoters"].indexOf(clientInfo["ip"]) > -1
+  // }
+  // finally {
+  //   postWasDownvoted = false
+  // }
+  // console.log(postObject)
+  // console.log(clientInfo)
+  postWasUpvoted = postObject["upvoters"].indexOf(clientInfo["ip"]) > -1
+  postWasDownvoted = postObject["downvoters"].indexOf(clientInfo["ip"]) > -1
+  if (postWasUpvoted) {
+    const iconInQuestion = document.getElementById(postObject["UUID"]).childNodes[0].childNodes[1].childNodes[1]
+    iconInQuestion.className = "message-board-post-rating-button-active"
+    iconInQuestion.style.color = "RGB(" + upvoteColor[0] + "," + upvoteColor[1] + "," + upvoteColor[2] + ")"
+  }
+  else if (postWasDownvoted) {
+    const iconInQuestion = document.getElementById(postObject["UUID"]).childNodes[0].childNodes[1].childNodes[5]
+    iconInQuestion.className = "message-board-post-rating-button-active"
+    iconInQuestion.style.color = "RGB(" + downvoteColor[0] + "," + downvoteColor[1] + "," + downvoteColor[2] + ")"
+  }
 }
 
 
 
-function addPostToMessageBoard(postObject) {
-  const newpost = '<div class = "message-board-post"><div class="message-board-post-question"> Q: ' + postObject["question"] + '</div><div class="message-board-post-answer">' + `${postObject["answer"] == "" ? "Unanswered..." : 'A: ' + postObject["answer"]}` + '</div></div>'
-
+function addPostToMessageBoard(postObject, throughSubmit) {
+  // const newpost = '<div class = "message-board-post"><div class="message-board-post-question"> Q: ' + postObject["question"] + '</div><div class="message-board-post-answer">' + `${postObject["answer"] == "" ? "Unanswered..." : 'A: ' + postObject["answer"]}` + '</div></div>'
+  // '<div class="message-board-post" id = "' + postObject["UUID"] + '"> <div class="message-board-post-rating-container"> <div class="message-board-post-rating"> <button class="message-board-post-rating-button" onclick = "handleVote(' + postObject["UUID"] + ',true)"> <i class="material-icons" style="font-size:24px;">thumb_up</i> </button> <div class="message-board-post-rating-score">0</div> <button class="message-board-post-rating-button" onclick = "handleVote(' + postObject["UUID"] + ',false)"> <i class="material-icons" style="font-size:24px;">thumb_down</i> </button> </div> </div> <div class="message-board-post-content"> <div class="message-board-post-question"> Q: ' + postObject["question"] + '</div> <div class="message-board-post-answer">' + `${postObject["answer"] == "" ? "Unanswered..." : 'A: ' + postObject["answer"]}` + '</div> </div ></div >'
+  const newpost = '<div class="message-board-post" id = "' + postObject["UUID"] + '"><div class="message-board-post-rating-container"> <div class="message-board-post-rating"> <button class="message-board-post-rating-button-inactive" onclick = "handleVote(\'' + postObject["UUID"] + '\',true)" > <i class="material-icons" style="font-size:24px;">thumb_up</i> </button > <div class="message-board-post-rating-score" id = "message-board-post-rating-score-' + postObject["UUID"] + '">' + postObject["postScore"] + '</div> <button class="message-board-post-rating-button-inactive" onclick="handleVote(\'' + postObject["UUID"] + '\',false)" > <i class="material-icons" style="font-size:24px;">thumb_down</i> </button > </div > </div > <div class="message-board-post-content"> <div class="message-board-post-question"> Q: ' + postObject["question"] + '</div> <div class="message-board-post-answer">' + `${postObject["answer"] == "" ? "Unanswered..." : 'A: ' + postObject["answer"]}` + '</div> </div ></div > '
+  if (throughSubmit) {
+    messageBoardPosts.push(postObject)
+  }
   if (messageBoardPosts.length == 0) {
     document.getElementById("message-board-posts").innerHTML = newpost;
-    messageBoardPosts.push(newpost)
   }
   else {
     document.getElementById("message-board-posts").innerHTML += newpost
   }
+
+  if (throughSubmit) {
+    renderActiveVoteButtons(postObject)
+  }
+
 
 }
 
@@ -59,15 +117,38 @@ function handleSubmit() {
   if (questionField.trim() == "") {
     return
   }
+  else if (numQuestionsToday >= 2) {
+    console.log("You've exceeded the number of questions today!")
+    return
+  }
   else {
 
     let post = {}
-    post["date"] = new Date().toGMTString()
+    currentDateObject = new Date()
+    post["date"] = currentDateObject.toGMTString()
+    post["dateValue"] = currentDateObject.valueOf()
     post["question"] = document.getElementById("submittor-question-field").value
+    post["UUID"] = generateUUID()
+    post["postScore"] = 1
+    post["upvoters"] = [clientInfo["ip"]]
+    post["downvoters"] = []
     console.log("Clicked!")
-    messageBoard.doc().set({ date: post["date"], question: post["question"] ,ip: clientInfo["ip"], answer: ""})
+    messageBoard.doc(post["UUID"]).set(
+      {
+        date: post["date"],
+        dateValue: post["dateValue"],
+        question: post["question"],
+        ip: clientInfo["ip"],
+        answer: "",
+        UUID: post["UUID"],
+        postScore: 1,
+        upvoters: [clientInfo["ip"]],
+        downvoters: []
+      }
+    )
 
-    addPostToMessageBoard(post)
+    addPostToMessageBoard(post, true)
+    numQuestionsToday += 1
   }
 }
 
@@ -81,34 +162,113 @@ async function DeleteAllPosts() {
     }).then(function () {
       messageBoardPosts = [];
       updateMessageBoard()
+      numQuestionsToday = 0
     })
 }
 
-function getClientInfo(){
+function getClientInfo() {
   console.log("Getting clientInfo...")
 
   url = 'https://json.geoiplookup.io/'
 
-  httpGetAsync(url, resp => {
-    clientInfo = JSON.parse(resp)
-  })
-}
+  var xmlHttp = new XMLHttpRequest();
 
-function httpGetAsync(theUrl, callback){
+  xmlHttp.onreadystatechange = () => {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      clientInfo = (JSON.parse(xmlHttp.responseText))
+  }
+  xmlHttp.open("GET", url, false); // true for asynchronous
+  xmlHttp.send(null);
 
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
 }
 
 
-function sameDay(d1, d2){
-  console.log("d1: ", d1, "\nd2: ",d2)
+
+function sameDay(d1, d2) {
+  // console.log("d1: ", d1, "\nd2: ", d2)
   return d1.getFullYear() === d2.getFullYear() &&
     d1.getMonth() === d2.getMonth() &&
     d1.getDate() === d2.getDate();
+}
+
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
+async function handleVote(postUUID, isUpvote) {
+  const multiplier = isUpvote ? 1 : -1;
+  let postIndex;
+  for (let index = 0; index < messageBoardPosts.length; index++) {
+    if (messageBoardPosts[index]["UUID"] == postUUID) {
+      postIndex = index
+      break;
+    }
+  }
+  // console.log("Clicked " + `${isUpvote ? "up" : "down"}` + "vote button on post:", postUUID, "@ index: ", postIndex)
+  if (isUpvote && messageBoardPosts[postIndex]["upvoters"].indexOf(clientInfo["ip"]) > -1) {
+    console.log("Cannot upvote twice!")
+    return
+  }
+  if (!isUpvote && messageBoardPosts[postIndex]["downvoters"].indexOf(clientInfo["ip"]) > -1) {
+    console.log("Cannot downvote twice!")
+    return
+  }
+
+  // await messageBoard.doc(postUUID).get().then((doc) => {
+  //   console.table(doc.data())
+  // })
+
+  newScore = messageBoardPosts[postIndex]["postScore"] + (1 * multiplier);
+  // console.table(messageBoardPosts[postIndex])
+
+  messageBoardPosts[postIndex]["postScore"] = newScore
+
+  upvoteIcon = document.getElementById(messageBoardPosts[postIndex]["UUID"]).childNodes[0].childNodes[1].childNodes[1]
+  downvoteIcon = document.getElementById(messageBoardPosts[postIndex]["UUID"]).childNodes[0].childNodes[1].childNodes[5]
+  if (isUpvote) {
+    if (messageBoardPosts[postIndex]["downvoters"].indexOf(clientInfo["ip"]) > -1) {
+      // post was previously downvoted, revert to zero, remove downvote class
+      messageBoardPosts[postIndex]["downvoters"].splice(messageBoardPosts[postIndex]["downvoters"].indexOf(clientInfo["ip"]), 1)
+      downvoteIcon.className = "message-board-post-rating-button-inactive"
+      downvoteIcon.style.color = ""
+    }
+    else {
+      // post wasn't previously downvoted, activate upvote class
+      messageBoardPosts[postIndex]["upvoters"].push(clientInfo["ip"])
+      upvoteIcon.className = "message-board-post-rating-button-active"
+      upvoteIcon.style.color = "RGB(" + upvoteColor[0] + "," + upvoteColor[1] + "," + upvoteColor[2] + ")"
+    }
+  }
+  else {
+    if (messageBoardPosts[postIndex]["upvoters"].indexOf(clientInfo["ip"]) > -1) {
+      // post was previously upvoted, revert to zero
+      // const indexOfUpvoteIP = messageBoardPosts[postIndex]["upvoters"].indexOf(clientInfo["ip"])
+      // console.log("post was previously upvoted, revert to zero, upvoteIP index: ", indexOfUpvoteIP)
+      messageBoardPosts[postIndex]["upvoters"].splice(messageBoardPosts[postIndex]["upvoters"].indexOf(clientInfo["ip"]), 1)
+      upvoteIcon.className = "message-board-post-rating-button-inactive"
+      upvoteIcon.style.color = ""
+    }
+    else {
+      console.log("post was never upvoted, adding to downvoters list")
+      messageBoardPosts[postIndex]["downvoters"].push(clientInfo["ip"])
+      downvoteIcon.className = "message-board-post-rating-button-active"
+      downvoteIcon.style.color = "RGB(" + downvoteColor[0] + "," + downvoteColor[1] + "," + downvoteColor[2] + ")"
+    }
+  }
+
+  document.getElementById("message-board-post-rating-score-" + postUUID).innerText = newScore
+
+  await messageBoard.doc(postUUID).update(
+    {
+      postScore: newScore,
+      upvoters: messageBoardPosts[postIndex]["upvoters"],
+      downvoters: messageBoardPosts[postIndex]["downvoters"],
+    }
+  )
+
+
 }
