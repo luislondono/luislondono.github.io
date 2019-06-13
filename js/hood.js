8a1519775dcf908bf5fc033a2be2166e58cecd4c
Luis Londono <luis.londono@uber.com>
2019-06-13 15:36:38 -0700

    Update hood.js

--- js/hood.js
+++ js/hood.js
@@ -24,30 +24,30 @@ function setupSpecificPage() {
     // provider.addScope('https://www.googleapis.com/auth/drive');
     firebase.auth().useDeviceLanguage()
 
-    // firebase.auth().signInWithPopup(provider).then(function (result) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // ...
-    // }).catch(function (error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    // });
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            console.log("Signed in !")
-        }
-    })
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //     if (user) {
+    //         console.log("Signed in !")
+    //     }
+    // })
 
-    firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().signInWithRedirect(provider);
 
     firebase.auth().getRedirectResult().then(function (result) {
         if (result.credential) {
