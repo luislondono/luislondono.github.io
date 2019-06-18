907d5b38feb2d6f012f750f74f5de87c3dd77f7c
Luis Londono <lhl49@cornell.edu>
2019-06-17 22:30:06 -0700

    added modal

--- js/investments.js
+++ js/investments.js
@@ -13,9 +13,16 @@ var firebaseConfig = {
 var userEmail;
 var userDocument;
 var portfolioCollection;
-var stockSearchBarElement;
+var stockSearchBarInputElement;
+var searchResultsContainerElement;
 var searchBarTimeout = null;
-var cashedQuery = null;
+var cachedQuery = null;
+var cachedLastSearchResultHTML = null
+var displayResultsContainer = false;
+var apiKeyCounter = 0;
+var modalActive = false;
+
+modal = '<div id="add-stock-from-search-modal" class="modal"> <div class="modal-container"> <button class="close" onclick="closeModal()">&times;</button> <div class="modal-content"> <p>Some text in the Modal..</p> </div> </div> </div>'
 
 
 
@@ -82,21 +89,52 @@ async function setupSpecificPage() {
     //     var credential = error.credential;
     //     // ...
     // });
-    stockSearchBarElement = document.getElementById('stock-search-bar')
-    stockSearchBarElement.onkeyup = function (e) {
+
+    stockSearchBarInputElement = document.getElementById('stock-search-bar')
+    searchResultsContainerElement = document.getElementsByClassName("search-results-container")[0];
+    stockSearchBarInputElement.onkeyup = function (e) {
         clearTimeout(searchBarTimeout);
         searchBarTimeout = setTimeout(function () {
             executeStockSearchQuery()
-        }, 500);
+        }, 100);
     }
 
 
+    document.getElementById("page-content").onclick = function (e){
+        // console.log(e.target)
+        if (document.getElementById('stock-search-container').contains(e.target)){
+            // Clicked in box
+            if (displayResultsContainer != true || stockSearchBarInputElement === document.activeElement){
+                displayResultsContainer = true
+                // console.log("Clicked in search container")
+                if (stockSearchBarInputElement.value != '' && stockSearchBarInputElement.value == cachedQuery){
+                    console.log("Pulling results from cache")
+                    searchResultsContainerElement.id = "";
+                    document.getElementsByClassName("stock-search-bar-container")[0].id = "stock-search-bar-container-with-results"
+                    searchResultsContainerElement.innerHTML = cachedLastSearchResultHTML
+                    // executeStockSearchQuery()
+                }
+            }
+          }
+        else{
+            console.log("Clicked outside search container")
+            escapeSearchField()
+        }
+    }
 
+}
+
+function escapeSearchField(){
+    displayResultsContainer = false
+    searchResultsContainerElement.innerHTML = ''
+    searchResultsContainerElement.id = "search-results-container-empty"
+    document.getElementsByClassName("stock-search-bar-container")[0].id = id="stock-search-bar-container-no-results"
 }
 
 function constructEndpoint(request) {
     var result = "https://www.alphavantage.co/query?function=" + request["function"]
-    const key = "8U6YN0U5W56PA5XE"
+    const key = ["PZIWIUXBWSES0VO4","U8NEQ8ZGZP25R6Z5","NYB2UDXDHZITQ0FZ","8U6YN0U5W56PA5XE", "SWUDD4PY4SEAT2O5"][apiKeyCounter]
+    apiKeyCounter += 1
 
 
     if (request["function"] == "GLOBAL_QUOTE") {
@@ -114,7 +152,7 @@ function constructEndpoint(request) {
 function queryAlphaVantage(request, callback) {
     // var xmlHttp = new XMLHttpRequest();
     const url = constructEndpoint(request)
-    console.log("Endpoint: ", url)
+    // console.log("Endpoint: ", url)
     fetch(url).then(function (response) {
         return response.json();
     }).then(function (json) {
@@ -124,7 +162,6 @@ function queryAlphaVantage(request, callback) {
 }
 
 function handleGetQuote() {
-
     req = {
         "function": "GLOBAL_QUOTE",
         "symbol": document.getElementById("symbol-field").value
@@ -133,7 +170,6 @@ function handleGetQuote() {
         document.getElementById("quote-response").innerText = JSON.stringify(json)
     }
     )
-
 }
 
 
@@ -180,29 +216,114 @@ function renderUserPortfolio() {
 }
 
 function executeStockSearchQuery() {
-    query = stockSearchBarElement.value
-    if (query != cashedQuery) {
-        cashedQuery = query
+    query = stockSearchBarInputElement.value.trim()
+    if (query != "" && query != cachedQuery) {
+        console.log("Continuing with requested query: ", query)
+        
         const req = {
             "function": "SYMBOL_SEARCH",
             "keywords": query
         }
         queryAlphaVantage(req, function (result) {
             console.log(result.bestMatches)
-            if (result != undefined) {
-                document.getElementsByClassName("search-results-container")[0].innerHTML = null
-                document.getElementsByClassName("search-results-container")[0].id = null;
+            cachedQuery = query
+            if (result.bestMatches != undefined && result.bestMatches.length != 0) {
+                console.log("Processing non-trivial results!")
+                resultHTMLAccumulator = ""
+                result.bestMatches.forEach(match => resultHTMLAccumulator += constructSearchMatchResultHTML(match))
+                if (resultHTMLAccumulator == ""){
+                    resultHTMLAccumulator = constructNoMatchResultsHTML()
+                }
+                cachedLastSearchResultHTML = resultHTMLAccumulator
+
+                if(displayResultsContainer || stockSearchBarInputElement === document.activeElement){
+                    searchResultsContainerElement.innerHTML = resultHTMLAccumulator
+                    document.getElementsByClassName("stock-search-bar-container")[0].id = "stock-search-bar-container-with-results"
+                    searchResultsContainerElement.id = "search-results-container-with-results";
+
+                }
+                
+            }
+            else{
                 document.getElementsByClassName("stock-search-bar-container")[0].id = "stock-search-bar-container-with-results"
-                result.bestMatches.forEach(match => renderSearchMatchResult(match))
+                searchResultsContainerElement.id = "search-results-container-with-results"
+                searchResultsContainerElement.innerHTML = constructNoMatchResultsHTML()
+                cachedLastSearchResultHTML = constructNoMatchResultsHTML()
             }
         })
+    }
+    else if (query == ""){
+        escapeSearchField()
     }
 
 }
 
-function renderSearchMatchResult(match) {
-    if (match["4. region"] == "United States") {
-        const resultElement = '<div class="search-result"> <h4 class="search-result-symbol">' + match["1. symbol"] + '</h4> <h4 class="search-result-company-name">' + match["2. name"] + '</h4> </div>'
-        document.getElementsByClassName("search-results-container")[0].insertAdjacentHTML("beforeend", resultElement)
+function constructNoMatchResultsHTML(){
+    return '<div class="search-result"> <h4 id="search-result-null"></h4> No results found </div>'
+}
+
+function constructSearchMatchResultHTML(match) {
+    if (match["4. region"] == "United States" && parseFloat(match["9. matchScore"]) > .5) {
+        const ticker = match["1. symbol"].trim()
+        const nameOffunction = `handleResultClick('`+ticker+`')`
+        return '<div class="search-result" id="search-result-'+ticker+'" onclick =' + nameOffunction+'> <h4 class="search-result-symbol">' + ticker + '</h4> <h4 class="search-result-company-name">' + match["2. name"] + '</h4> </div>'
+        // searchResultsContainerElement.insertAdjacentHTML("beforeend", resultElement)
+    }
+    return ""
+}
+
+function handleResultClick(symbol){
+    // console.log("You want to add ", symbol, " to your portfolio")
+    const searchResult = document.getElementById("search-result-" + symbol)
+    generateModal(modal,function(){
+
+        
+        document.getElementById("portfolio-list-container").insertAdjacentHTML("beforeend", generatePortfolioPositionHTML(searchResult.children[1].innerHTML,searchResult.children[0].innerHTML,1))
+        
+        
+        escapeSearchField()
+        req = {
+            "function": "GLOBAL_QUOTE",
+            "symbol": symbol
+        }
+        
+        let currentPrice;
+        queryAlphaVantage(req, function (json) {
+            console.log(json)
+            currentPrice = Math.ceil(parseFloat( json["Global Quote"]["05. price"]) * 100) / 100
+            document.getElementById("stock-container-" + symbol).children[1].innerText = '$' + currentPrice
+        })
+        
+        userDocument.latestPortfolio[symbol] = {
+            "companyName" : searchResult.children[1],
+            "price": currentPrice,
+            "shares" : 1
+        } 
+        
+    })
+}
+
+function generatePortfolioPositionHTML(companyName, ticker, numShares){
+    // return '<div class="stock-container" id= "stock-container-'+ticker+'"> <div class="company-info"> <span> <h4 class="company-info-name">'+companyName+'</h4> <h4><h4>&nbsp$</h4><h4 class="company-info-ticker">'+ticker+'</h4></h4> </span> <h5 class="num-shares-label">'+numShares+'&nbspshare'+ (numShares>1? 's':'')+'</h5> </div> <div class="company-price-container"> <h4>Price</h4> </div> </div>'
+    return '<div class="stock-container" id= "stock-container-'+ticker+'"> <div class="company-info"> <span> <h4 class="company-info-name">'+companyName+'</h4> <h4><h4>&nbsp$</h4><h4 class="company-info-ticker">'+ticker+'</h4></h4> </span> <h5 class="num-shares-label"><input type="number" value = "'+numShares+'"> &nbspshare'+ (numShares>1? 's':'')+'</h5> </div> <div class="company-price-container"> <h4></h4> </div> </div>'
+}
+
+function generateModal(element = modal, callback){
+    console.log(element)
+    document.getElementById("page-content").insertAdjacentHTML("afterend",element)
+    setTimeout(()=>{window.onclick = windowOnClickForActiveModal},10)
+    
+}
+
+function closeModal(){
+    document.getElementById("add-stock-from-search-modal").remove()
+    window.onclick = null
+}
+
+function windowOnClickForActiveModal(event) {
+    var modal = document.getElementById("add-stock-from-search-modal")
+    if (!(modal === null)){
+        console.log("Clicked out of modal")
+        closeModal()
     }
 }
