da676832753f778d41d60dadbba5d2ca67c9e1a9
Luis Londono <luis.londono@uber.com>
2019-07-17 11:06:40 -0700

    Update checkers.js

--- js/checkers.js
+++ js/checkers.js
@@ -11,6 +11,8 @@ var firebaseConfig = {
 var gameInitializerContainer;
 var boardState;
 var piecesState = {};
+var primaryPieceCount = 0
+var secondaryPieceCount = 0
 
 setupSpecificPage = function () {
     console.log("Setting up Checkers.html")
@@ -32,87 +34,51 @@ function drawCheckersBoard() {
     var checkersBoard = document.createElement('div')
     checkersBoard.classList.add("checkers-board")
 
-    primaryPieceCount = 0
-    secondaryPieceCount = 0
+
 
 
     for (let row = 0; row < 8; row++) {
         var checkersRow = document.createElement('div')
         checkersRow.classList.add("checkers-row")
         for (let square = 0; square < 8; square++) {
-            var checkersSquare = document.createElement('div')
-            checkersSquare.classList.add("checkers-square")
-            // console.log(`row: ${row % 2} , square: ${square % 2}`)
-            var checker = document.createElement('button')
-            checker.classList.add("checker")
+            var checkersSquare = createCheckerSquare(square, 7 - row)
+            // var checkersSquare = document.createElement('div')
+            // checkersSquare.classList.add("checkers-square")
 
             switch (row) {
                 case 0:
-                    if (square % 2 == 0) {
-                        checker.classList.add("primary-checker")
-                        checker.id = "primary-checker-" + primaryPieceCount
-                        piecesState[checker.id] = [row, square]
-                        checker.addEventListener('click', function (e) {
-                            handleSelectPiece(e.target)
-                        })
-                        primaryPieceCount += 1
-                        checkersSquare.appendChild(checker)
+                    if (square % 2 == 1) {
+                        createChecker(false, checkersSquare, square, 7 - row)
                     }
                     break;
                 case 1:
+                    if (square % 2 == 0) {
+                        createChecker(false, checkersSquare, square, 7 - row)
+                    }
+                    break;
+                case 2:
                     if (square % 2 == 1) {
-                        checker.classList.add("primary-checker")
-                        checker.id = "primary-checker-" + primaryPieceCount
-                        piecesState[checker.id] = [row, square]
-                        checker.addEventListener('click', function (e) {
-                            handleSelectPiece(e.target)
-                        })
-                        primaryPieceCount += 1
-                        checkersSquare.appendChild(checker)
+                        createChecker(false, checkersSquare, square, 7 - row)
+                    }
+                    break;
+                case 5:
+                    if (square % 2 == 0) {
+                        createChecker(true, checkersSquare, square, 7 - row)
                     }
                     break;
                 case 6:
-                    if (square % 2 == 0) {
-                        checker.classList.add("secondary-checker")
-                        checker.id = "secondary-checker-" + secondaryPieceCount
-                        piecesState[checker.id] = [row, square]
-                        checker.addEventListener('click', function (e) {
-                            handleSelectPiece(e.target)
-                        })
-                        secondaryPieceCount += 1
-                        checkersSquare.appendChild(checker)
+                    if (square % 2 == 1) {
+                        createChecker(true, checkersSquare, square, 7 - row)
                     }
                     break;
                 case 7:
-                    if (square % 2 == 1) {
-                        checker.classList.add("secondary-checker")
-                        checker.id = "secondary-checker-" + secondaryPieceCount
-                        piecesState[checker.id] = [row, square]
-                        checker.addEventListener('click', function (e) {
-                            handleSelectPiece(e.target)
-                        })
-                        secondaryPieceCount += 1
-                        checkersSquare.appendChild(checker)
+                    if (square % 2 == 0) {
+                        createChecker(true, checkersSquare, square, 7 - row)
                     }
                     break;
             }
 
-            if (row % 2 == 0) {
-                if (square % 2 == 0) {
-                    checkersSquare.classList.add("primary-square-2")
-                }
-                else {
-                    checkersSquare.classList.add("secondary-square-2")
-                }
-            }
-            else {
-                if (square % 2 == 0) {
-                    checkersSquare.classList.add("secondary-square-2")
-                }
-                else {
-                    checkersSquare.classList.add("primary-square-2")
-                }
-            }
+
             checkersRow.appendChild(checkersSquare)
         }
         checkersBoard.appendChild(checkersRow)
@@ -123,5 +89,109 @@ function drawCheckersBoard() {
 }
 
 function handleSelectPiece(checkerDiv) {
-    console.log(event.target)
+    // console.log(event.target)
+    checkerID = event.target.id
+    checkerPosition = piecesState[checkerID]
+
+    console.log(`checkerID : ${checkerID}, position:(${piecesState[checkerID][0]},${piecesState[checkerID][1]})`)
+    // console.log(`Possible Moves: ${genPossibleMoves(checkerPosition)}`)
+
+    // if checkerID
+    moves = genPossibleMoves(checkerPosition)
+    // console.log(moves)
+    moves.forEach(coord => {
+        toggleTileHighlight(coord)
+    })
+
+}
+
+function genPossibleMoves(position) {
+    result = []
+    // North-West
+    nwPosition = [position[0] - 1, position[1] - 1]
+    // NortiEast
+    nePosition = [position[0] + 1, position[1] + 1]
+    // SoutiWest
+    swPosition = [position[0] - 1, position[1] - 1]
+    // SoutiEast
+    sePosition = [position[0] + 1, position[1] - 1]
+    positions = [
+        [position[0] - 1, position[1] - 1],
+        [position[0] + 1, position[1] - 1],
+        [position[0] - 1, position[1] + 1],
+        [position[0] + 1, position[1] + 1],
+    ]
+    positions.forEach(position => {
+        if (isValidCoordinate(position) && (pieceAtPosition(position) == null)) {
+            result.push(position)
+        }
+    });
+
+    return result
+
+}
+
+function isValidCoordinate(coord) {
+    return coord[0] <= 7 && coord[0] >= 0 && coord[1] <= 7 && coord[1] >= 0
+}
+
+function pieceAtPosition(targetCoord) {
+    for (const piece in piecesState) {
+        if (piecesState.hasOwnProperty(piece)) {
+            const pieceCoord = piecesState[piece];
+            // console.log(pieceCoord)
+            if (pieceCoord[0] == targetCoord[0] && pieceCoord[1] == targetCoord[1]) {
+                return piece
+            }
+        }
+    }
+    return null
+}
+
+function createChecker(isPrimary, checkerSquareElement, x, y) {
+    var checker = document.createElement('button')
+    checker.classList.add("checker")
+    subClassName = isPrimary ? "primary-checker" : "secondary-checker"
+    checker.classList.add(subClassName)
+
+    checkerNumber = isPrimary ? primaryPieceCount : secondaryPieceCount
+    if (isPrimary) { primaryPieceCount += 1 }
+    else { secondaryPieceCount += 1 }
+    checker.id = subClassName + checkerNumber
+
+    piecesState[checker.id] = [x, y]
+
+    checker.addEventListener('click', function (e) {
+        handleSelectPiece(e.target)
+    })
+    checkerSquareElement.appendChild(checker)
+}
+
+function createCheckerSquare(x, y) {
+    var checkersSquare = document.createElement('div')
+    checkersSquare.classList.add("checkers-square")
+    if (y % 2 == 0) {
+        if (x % 2 == 0) {
+            checkersSquare.classList.add("primary-square-2")
+        }
+        else {
+            checkersSquare.classList.add("secondary-square-2")
+        }
+    }
+    else {
+        if (x % 2 == 0) {
+            checkersSquare.classList.add("secondary-square-2")
+        }
+        else {
+            checkersSquare.classList.add("primary-square-2")
+        }
+    }
+
+    checkersSquare.id = `checkers-square-${x}-${y}`
+    return checkersSquare
+}
+
+function toggleTileHighlight(coord) {
+    tile = document.getElementById(`checkers-square-${coord[0]}-${coord[1]}`)
+    tile.style.border = (tile.style.border == "3px solid red") ? "" : "3px solid red"
 }
